5d2014acbed396ba0ce40e0b1f26bb978fdb340c
Tao Tang <derektaotang@gmail.com>
2016-04-05 19:47:46 -0400

    tt

---
+++ src/main/java/com/myWalmart/app/WalmartTicketProjectTest/InitializeStadiumTest.java
@@ -0,0 +1,141 @@
+package com.myWalmart.app.WalmartTicketProjectTest;
+
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.myWalmart.app.ticketServiceData.Seat;
+import com.myWalmart.app.ticketServiceImp.InitializeStadium;
+
+
+public class InitializeStadiumTest {
+
+	InitializeStadium is;
+	int numberOfLevels;
+	int rowOnOrchestra;
+	int seatNumOrchestra;
+	int rowOnMain;
+	int seatNumMain;
+	int rowOnBalconyOne;
+	int seatNumBalconyOne;
+	int rowOnBalconyTwo;
+	int seatNumBalconyTwo;
+	double priceOrchestra;
+	double priceMain;
+	double priceBalconyOne;
+	double priceBalconyTwo;
+
+	@BeforeTest
+	public void setup() {
+		is = new InitializeStadium();
+		is.initializedSeats();
+		numberOfLevels = InitializeStadium.numberOfLevels;
+		rowOnOrchestra = InitializeStadium.rowOnOrchestra;
+		seatNumOrchestra = InitializeStadium.seatNumOrchestra;
+		rowOnMain = InitializeStadium.rowOnMain;
+		seatNumMain = InitializeStadium.seatNumMain;
+		rowOnBalconyOne = InitializeStadium.rowOnBalconyOne;
+		seatNumBalconyOne = InitializeStadium.seatNumBalconyOne;
+		rowOnBalconyTwo = InitializeStadium.rowOnBalconyTwo;
+		seatNumBalconyTwo = InitializeStadium.seatNumBalconyTwo;
+		priceOrchestra = InitializeStadium.priceOrchestra;
+		priceMain = InitializeStadium.priceMain;
+		priceBalconyOne = InitializeStadium.priceBalconyOne;
+		priceBalconyTwo = InitializeStadium.priceBalconyTwo;
+
+	}
+
+	@Test
+	public void testInitializedSeats() {
+		ArrayList<Seat[][]> seats = is.initializedSeats();
+		int levelofStadium = 4;
+		// test not null
+		Assert.assertNotNull(is.initializedSeats());
+		// test the stadium has 4 levels
+		Assert.assertEquals(levelofStadium, seats.size());
+
+	}
+
+	@Test
+	public void testSeats() {
+		ArrayList<Seat[][]> seats = is.initializedSeats();
+		for (int i = 0; i < seats.size(); i++) {
+			Seat[][] level = seats.get(i);
+
+			if (i == 0) {
+				Assert.assertEquals(level.length, rowOnOrchestra);// test for
+																	// Orchestra
+																	// level
+				for (int j = 0; j < level.length; j++) {
+					int seatNum = level[0].length; // seat numbers for this
+													// level
+					for (int k = 0; k < seatNum; k++) {
+						Seat currentS = level[j][k];
+						Assert.assertEquals(currentS.getLevelName(), "Orchestra");
+						Assert.assertEquals(currentS.getPrice(), Double.valueOf(priceOrchestra));
+
+					}
+					Assert.assertEquals(seatNum, seatNumOrchestra);
+				}
+
+			}
+
+			if (i == 1) {
+				Assert.assertEquals(level.length, rowOnMain);// test for
+																// Orchestra
+																// level
+				for (int j = 0; j < level.length; j++) {
+					int seatNum = level[0].length; // seat numbers for this
+													// level
+					for (int k = 0; k < seatNum; k++) {
+						Seat currentS = level[j][k];
+						Assert.assertEquals(currentS.getLevelName(), "Main");
+						Assert.assertEquals(currentS.getPrice(), Double.valueOf(priceMain));
+
+					}
+					Assert.assertEquals(seatNum, seatNumMain);
+				}
+
+			}
+
+			if (i == 2) {
+				Assert.assertEquals(level.length, rowOnBalconyOne);// test for
+																	// Orchestra
+																	// level
+				for (int j = 0; j < level.length; j++) {
+					int seatNum = level[0].length; // seat numbers for this
+													// level
+					for (int k = 0; k < seatNum; k++) {
+						Seat currentS = level[j][k];
+						Assert.assertEquals(currentS.getLevelName(), "BalconyOne");
+						Assert.assertEquals(currentS.getPrice(), Double.valueOf(priceBalconyOne));
+
+					}
+					Assert.assertEquals(seatNum, seatNumBalconyOne);
+				}
+
+			}
+
+			if (i == 3) {
+				Assert.assertEquals(level.length, rowOnBalconyTwo);// test for
+																	// Orchestra
+																	// level
+				for (int j = 0; j < level.length; j++) {
+					int seatNum = level[0].length; // seat numbers for this
+													// level
+					for (int k = 0; k < seatNum; k++) {
+						Seat currentS = level[j][k];
+						Assert.assertEquals(currentS.getLevelName(), "BalconyTwo");
+						Assert.assertEquals(currentS.getPrice(), Double.valueOf(priceBalconyTwo));
+
+					}
+					Assert.assertEquals(seatNum, seatNumBalconyTwo);
+				}
+
+			}
+		}
+	}
+
+}
---
+++ src/main/java/com/myWalmart/app/ticketServiceImp/InitializeStadium.java
@@ -0,0 +1,114 @@
+package com.myWalmart.app.ticketServiceImp;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.ejb.Singleton;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.myWalmart.app.ticketServiceData.Seat;
+import com.myWalmart.app.ticketServiceData.StadiumLevel;
+
+public class InitializeStadium {
+
+	public static Integer numberOfLevels;
+	public static Integer rowOnOrchestra;
+	public static Integer seatNumOrchestra;
+	public static Integer rowOnMain;
+	public static Integer seatNumMain;
+	public static Integer rowOnBalconyOne;
+	public static Integer seatNumBalconyOne;
+	public static Integer rowOnBalconyTwo;
+	public static Integer seatNumBalconyTwo;
+	public static Double priceOrchestra;
+	public static Double priceMain;
+	public static Double priceBalconyOne;
+	public static Double priceBalconyTwo;
+	public static String orchestraName;
+	public static String mainName;
+	public static String balconyOneName;
+	public static String balconyTwoName;
+	public static ArrayList<Seat[][]>  newStadium;
+
+	@SuppressWarnings("null")
+	public ArrayList<Seat[][]> initializedSeats() {
+		JSONParser parser = new JSONParser();
+		try {
+			Object obj = parser.parse(new FileReader("seatsInformation.json"));
+
+			JSONObject jsonObject = (JSONObject) obj;
+
+			orchestraName = (String) jsonObject.get("orchestraName");
+
+			mainName = (String) jsonObject.get("mainName");
+
+			balconyOneName = (String) jsonObject.get("balconyOneName");
+
+			balconyTwoName = (String) jsonObject.get("balconyTwoName");
+
+			numberOfLevels = Integer.valueOf((String) jsonObject.get("numberOfLevels"));
+
+			rowOnOrchestra = Integer.valueOf((String) jsonObject.get("rowOnOrchestra"));
+
+			rowOnMain = Integer.valueOf((String) jsonObject.get("rowOnMain"));
+
+			rowOnBalconyOne = Integer.valueOf((String) jsonObject.get("rowOnBalconyOne"));
+
+			rowOnBalconyTwo = Integer.valueOf((String) jsonObject.get("rowOnBalconyTwo"));
+
+			seatNumOrchestra = Integer.valueOf((String) jsonObject.get("seatNumOrchestra"));
+
+			seatNumMain = Integer.valueOf((String) jsonObject.get("seatNumMain"));
+
+			seatNumBalconyOne = Integer.valueOf((String) jsonObject.get("seatNumBalconyOne"));
+
+			seatNumBalconyTwo = Integer.valueOf((String) jsonObject.get("seatNumBalconyTwo"));
+
+			priceOrchestra = Double.valueOf((String) jsonObject.get("priceOrchestra"));
+
+			priceMain = Double.valueOf((String) jsonObject.get("priceMain"));
+			priceBalconyOne = Double.valueOf((String) jsonObject.get("priceBalconyOne"));
+			priceBalconyTwo = Double.valueOf((String) jsonObject.get("priceBalconyTwo"));
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println(" Failed not found!");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		ArrayList<StadiumLevel> levels = new ArrayList<StadiumLevel>();
+		StadiumLevel orchestraLevel = new StadiumLevel(rowOnOrchestra, seatNumOrchestra, orchestraName, priceOrchestra);
+		levels.add(orchestraLevel);
+		StadiumLevel mainLevel = new StadiumLevel(rowOnMain, seatNumMain, mainName, priceMain);
+		levels.add(mainLevel);
+		StadiumLevel balconyOneLevel = new StadiumLevel(rowOnBalconyOne, seatNumBalconyOne, balconyOneName,
+				priceBalconyOne);
+		levels.add(balconyOneLevel);
+		StadiumLevel balconyTwoLevel = new StadiumLevel(rowOnBalconyTwo, seatNumBalconyTwo, balconyTwoName,
+				priceBalconyTwo);
+		levels.add(balconyTwoLevel);
+
+		ArrayList<Seat[][]> allSeats = new ArrayList<Seat[][]>();
+
+		for (int p = 0; p < levels.size(); p++) {
+			StadiumLevel level = levels.get(p);
+			Seat[][] levelSeats = new Seat[level.getRowNum()][level.getSeatNum()];
+			for (int j = 0; j < level.getRowNum(); j++) {
+				for (int k = 0; k < level.getSeatNum(); k++) {
+					levelSeats[j][k] = new Seat(0, j, k, level.getLevelPrice(), level.getLevelName());
+				}
+			}
+			allSeats.add(levelSeats);
+		}
+		newStadium = allSeats;
+		return allSeats;
+	}
+
+}
